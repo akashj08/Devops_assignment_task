092ca737c82542a97d7738fb745bfeef99b5b730
Akash Jaiswal <ajaiswal@noon.com>
2021-04-10 16:08:43 +0530

    updated

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -90,3 +90,102 @@ resource "aws_nat_gateway" "default" {
   subnet_id     = aws_subnet.public[count.index].id
 }
  
+
+
+/*
+  Web Servers
+*/
+resource "aws_security_group" "public_sg" {
+    name = "vpc_sg"
+    description = "Allow incoming HTTP connections."
+
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress { # SQL Server
+        from_port = -1
+        to_port = -1
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    vpc_id = "${aws_vpc.default.id}"
+
+    
+}
+
+resource "aws_instance" "public-app-1" {
+    ami = "${var.ami_id}"
+    availability_zone = "us-east-2a"
+    instance_type = "m1.small"
+    key_name = "${var.aws_key_name}"
+    vpc_security_group_ids = ["${aws_security_group.public_sg.id}"]
+    subnet_id = "${aws_subnet.public.id}"
+    associate_public_ip_address = true
+    source_dest_check = false
+    
+
+}
+
+resource "aws_eip" "public-app-1" {
+    instance = "${aws_instance.public-app-1.id}"
+    vpc = true
+}
+
+
+/*
+  Private Servers
+*/
+resource "aws_security_group" "private_sg" {
+    name = "vpc_db"
+    description = "Allow incoming database connections."
+
+    ingress { # SQL Server
+        from_port = -1
+        to_port = -1
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.public_sg.id}"]
+    }
+    
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["${var.cidr_block}"]
+    }
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["${var.cidr_block}"]
+    }
+
+    egress {
+        from_port = -1
+        to_port = -1
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+
+    vpc_id = "${aws_vpc.default.id}"
+
+    
+}
+
+resource "aws_instance" "private-app-1" {
+    ami = "${var.ami_id}"
+    availability_zone = "us-east-2a"    
+    instance_type = "m1.small"
+    key_name = "${var.aws_key_name}"
+    vpc_security_group_ids = ["${aws_security_group.private_sg.id}"]
+    subnet_id = "${aws_subnet.private.id}"
+    source_dest_check = false
+
+    
+}
+
