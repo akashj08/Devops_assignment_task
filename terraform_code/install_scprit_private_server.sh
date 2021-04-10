81853933233bdbc066e531f939ad2075e8183532
Akash Jaiswal <ajaiswal@noon.com>
2021-04-11 00:10:29 +0530

    updated

--- terraform_code/install_scprit_private_server.sh
+++ terraform_code/install_scprit_private_server.sh
@@ -2,9 +2,5 @@
 #scrpit run
 #install docker 
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce
+sudo apt install docker.io
 sudo usermod -aG docker ${USER}
