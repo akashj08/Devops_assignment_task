ecb00d16e33f9525b93557bc916a8fbc1bc936ba
Akash Jaiswal <ajaiswal@noon.com>
2021-04-10 17:17:22 +0530

    updated

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -121,7 +121,7 @@ resource "aws_security_group" "public_sg" {
 resource "aws_instance" "public-app-1" {
     count = length(var.public_subnet_cidr_blocks)
     ami = "${var.ami_id}"
-    availability_zone = "us-east-2a"
+    availability_zone = "us-east-2b"
     instance_type = "m1.small"
     key_name = "${var.aws_key_name}"
     vpc_security_group_ids = ["${aws_security_group.public_sg.id}"]
