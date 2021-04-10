b9674096fcce1421002d258205b954728e466d55
Akash Jaiswal <ajaiswal@noon.com>
2021-04-10 23:31:16 +0530

    updated

--- terraform_code/install_scprit_public_server.sh
+++ terraform_code/install_scprit_public_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#install java8
+sudo apt update
+sudo apt install openjdk-8-jdk openjdk-8-jre -y
+
+#install jenkins
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo add-apt-repository universe
