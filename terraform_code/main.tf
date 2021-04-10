6673451537e5aee4a3adfc3262e88623cff6cbaa
Akash Jaiswal <ajaiswal@noon.com>
2021-04-10 16:44:02 +0530

    updated

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -133,7 +133,7 @@ resource "aws_instance" "public-app-1" {
 }
 
 resource "aws_eip" "public-app-1" {
-    instance = aaws_instance.public-app-1[count.index]
+    instance = aws_instance.public-app-1[count.index]
     vpc = true
 }
 
