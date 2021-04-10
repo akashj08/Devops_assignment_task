a67a1c36b820e9af4534467a81e94f3b718b8cc4
Akash Jaiswal <ajaiswal@noon.com>
2021-04-10 14:36:56 +0530

    added code

--- terraform_code/aws.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-    shared_credentials_file = "/home/ubuntu/.aws/creds"
-    profile = "default"
-    region = "${var.aws_region}"
-}
--- terraform_code/variables.tf
+++ terraform_code/variables.tf
@@ -1,34 +1,37 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_key_path" {}
-variable "aws_key_name" {
-  description = "EC2 Region for the VPC"
-    default = "gaurav"
+
+variable "secret_key" {
+     description = "Secret key to AWS console"
+     
 }
 
-variable "aws_region" {
-    description = "EC2 Region for the VPC"
-    default = "eu-west-1"
+variable "region" {
+  default     = "eu-west-3"
+  type        = string
+  description = "Region of the VPC"
 }
 
-variable "amis" {
-    description = "AMIs by region"
-    default = {
-        eu-west-1 = "ami-f1810f86" # ubuntu 14.04 LTS
-    }
+
+variable "cidr_block" {
+  default     = "10.0.0.0/16"
+  type        = string
+  description = "CIDR block for the VPC"
 }
 
-variable "vpc_cidr" {
-    description = "CIDR for the whole VPC"
-    default = "10.0.0.0/16"
+variable "public_subnet_cidr_blocks" {
+  default     = ["10.0.0.0/24", "10.0.2.0/24"]
+  type        = list
+  description = "List of public subnet CIDR blocks"
 }
 
-variable "public_subnet_cidr" {
-    description = "CIDR for the Public Subnet"
-    default = "10.0.0.0/24"
+variable "private_subnet_cidr_blocks" {
+  default     = ["10.0.1.0/24", "10.0.3.0/24"]
+  type        = list
+  description = "List of private subnet CIDR blocks"
 }
 
-variable "private_subnet_cidr" {
-    description = "CIDR for the Private Subnet"
-    default = "10.0.1.0/24"
+variable "availability_zones" {
+  default     = ["eu-west-3a", "eu-west-3b"]
+  type        = list
+  description = "List of availability zones"
 }
+ 
